cb1c9d1aa0a8e3d415c1a80bc339674c023a1cb8
NPAL srl <nicola@npal.it>
2025-10-21 08:16:40 +0200

    commit

--- product_multi_barcode/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import product_barcode
-from . import product_product
-from . import product_template
-from . import sale_order_line
-from . import purchase_order_line
-from . import stock_move
